0fa089b6a9d3f65acba378dad9399515349b85d0
Jason Kang <jasonkang14@gmail.com>
2024-05-22 15:19:17 +0900

    Fix the style of copy button

--- st_copy_to_clipboard/frontend/main.js
+++ st_copy_to_clipboard/frontend/main.js
@@ -4,7 +4,7 @@
 // means you're missing that file.
 
 function sendValue(value) {
-  Streamlit.setComponentValue(value)
+  Streamlit.setComponentValue(value);
 }
 
 /**
@@ -15,44 +15,32 @@ function sendValue(value) {
 function onRender(event) {
   // Only run the render code the first time the component is loaded.
   if (!window.rendered) {
-    const { text, before_copy_label, after_copy_label, show_text } = event.detail.args;
+    const { text, before_copy_label, after_copy_label, show_text } =
+      event.detail.args;
 
-    const container = document.querySelector('#container');
-    const button = document.querySelector('#copy-button');
-    const textElement = document.querySelector('#text-element');
-
-    button.textContent = before_copy_label;  // Set initial label
-
-    // Show text if show_text is true
-    if (show_text) {
-      textElement.textContent = text;
-      textElement.style.display = 'inline';
-    } else {
-      textElement.style.display = 'none';
-    }
+    const container = document.querySelector("#container");
+    const button = document.querySelector("#copy-button");
 
     function copyToClipboard() {
       navigator.clipboard.writeText(text);
 
-      button.textContent = after_copy_label;  // Change label after copying
+      button.textContent = after_copy_label; // Change label after copying
 
       setTimeout(() => {
         if (!button) return;
-        button.textContent = before_copy_label;  // Revert to original label after 1 second
+        button.textContent = before_copy_label; // Revert to original label after 1 second
       }, 1000);
     }
 
-    button.addEventListener('click', copyToClipboard);
-    textElement.addEventListener('click', copyToClipboard);
+    button.addEventListener("click", copyToClipboard);
 
     window.rendered = true;
   }
 }
 
-
 // Render the component whenever python send a "render event"
-Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender)
+Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, onRender);
 // Tell Streamlit that the component is ready to receive events
-Streamlit.setComponentReady()
+Streamlit.setComponentReady();
 // Render with the correct height, if this is a fixed-height component
-Streamlit.setFrameHeight(100)
+Streamlit.setFrameHeight(100);
